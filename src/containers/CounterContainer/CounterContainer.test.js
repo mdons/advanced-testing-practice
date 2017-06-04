944f674eb401cd9e43902fa4e3363541cf56d9b1
Jon Woo <jw56578@gmail.com>
2017-06-04 13:21:38 -0500

    add tests

---
+++ src/containers/CounterContainer/CounterContainer.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {mount } from 'enzyme';
+import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import store from "../../store";
+import CounterContainer from "./";
+
+describe('Counter Dispaly', () => {
+  it('Shows the currentCount', () => {
+    const wrapper = mount(<Provider store={store}><CounterContainer /></Provider>);
+    
+    store.dispatch({
+      type:"INCREASE_COUNTER"
+    });
+    var div = wrapper.find("div").at(0);
+    expect(div.text()).toBe("Counter: 1");
+
+    store.dispatch({
+      type:"INCREASE_COUNTER"
+    });
+    var div = wrapper.find("div").at(0);
+    expect(div.text()).toBe("Counter: 2");
+
+    store.dispatch({
+      type:"INCREASE_COUNTER"
+    });
+    var div = wrapper.find("div").at(0);
+    expect(div.text()).toBe("Counter: 3");
+
+    store.dispatch({
+      type:"DECREASE_COUNTER"
+    });
+    var div = wrapper.find("div").at(0);
+    expect(div.text()).toBe("Counter: 2");
+
+
+  });
+});
